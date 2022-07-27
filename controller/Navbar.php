a32aac48e7446b0a4edce551e12b35c615318488
Ahmed Samir <a.samirsherif@gmail.com>
2022-07-27 03:42:31 +0200

    fix Navbar return error when recieve empty data

--- controller/Navbar.php
+++ controller/Navbar.php
@@ -4,10 +4,15 @@ class Navbar extends Controller {
     public function initCategories() {
         $categories = $this->model->getAllCategories();
         $newCategories = array();
-        
-        foreach($categories as $category) {
-            $newCategories[$category['id']] = $category['category_name'];
+
+        if($categories){
+            foreach($categories as $category) {
+                $newCategories[$category['id']] = $category['category_name'];
+            } 
+        }else{
+            $newCategories = [];
         }
+        
 
         Session::set('categories', $newCategories);
         Session::set('activeCategory', 'null');
