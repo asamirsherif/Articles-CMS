9b731642d39f9bc4e3897d5b79ad57f4ccf3ac4b
Ahmed Samir <a.samirsherif@gmail.com>
2022-07-27 03:43:15 +0200

    fix for footer

--- view/layout/footer_essentials.php
+++ view/layout/footer_essentials.php
@@ -1,7 +1,7 @@
 
-<!-- first jquery, then popper, then bootstrap -->
-<script src="public\lib\bootstrap\bootstrap.bundle.min.js"></script>
-    
+<!-- first jquery, then bootstrap -->
+<script src="public/lib/jquery/jquery-3.1.1.min.js"></script>
+<script src="public/lib/bootstrap/bootstrap.bundle.js"></script>
 <script src="<?=URL?>public/lib/tinymce/tinymce-4.8.1.min.js"></script>
 
 <script src="<?=URL?>public/js/scripts.js"></script>
